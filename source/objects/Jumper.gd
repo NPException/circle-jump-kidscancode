099266b93079463d6102f123fcdfb2ada2f075b4
NPException <npexception42@gmail.com>
2019-05-22 22:16:30 +0200

    Video Part 2

--- source/objects/Jumper.gd
+++ source/objects/Jumper.gd
@@ -1,22 +1,34 @@
 extends Area2D
+class_name Jumper
+
+signal captured
+
+onready var trail := $Trail/Points
 
 var velocity := Vector2(100, 0)
 var jump_speed := 1000
-var target : Area2D = null
+var target : Circle = null
+var trail_length := 25
 
 func _unhandled_input(event):
 	if target and event is InputEventScreenTouch and event.pressed:
 		jump()
 
 func jump():
+	target.implode()
 	target = null
 	velocity = transform.x * jump_speed
 
 func _on_Jumper_area_entered(area):
 	target = area
+	target.get_node("Pivot").rotation = (position - target.position).angle()
 	velocity = Vector2.ZERO
+	emit_signal("captured", area)
 
 func _process(delta):
+	if trail.points.size() > trail_length:
+		trail.remove_point(0)
+	trail.add_point(position)
 	if target:
 		transform = target.orbit_position.global_transform
 	else:
