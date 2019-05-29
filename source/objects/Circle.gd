46825569934080b4f6e6ffd91332686f2cb0d8ee
NPException <npexception42@gmail.com>
2019-05-29 22:13:52 +0200

    Video Part 6
    - add audio
    - start theme system

--- source/objects/Circle.gd
+++ source/objects/Circle.gd
@@ -17,6 +17,8 @@ func init(_position, _radius = radius, _mode = Mode.LIMITED):
 	set_mode(_mode)
 	position = _position
 	radius = _radius
+	$Sprite.material = $Sprite.material.duplicate()
+	$SpriteEffect.material = $Sprite.material
 	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate()
 	$CollisionShape2D.shape.radius = radius
 	var img_size = $Sprite.texture.get_size().x / 2
@@ -26,13 +28,17 @@ func init(_position, _radius = radius, _mode = Mode.LIMITED):
 
 func set_mode(_mode : int):
 	mode = _mode
+	var color
 	match mode:
 		Mode.STATIC:
 			$Label.hide()
+			color = settings.theme["circle_static"]
 		Mode.LIMITED:
 			current_orbits = num_orbits
 			$Label.text = str(current_orbits)
 			$Label.show()
+			color = settings.theme["circle_limited"]
+	$Sprite.material.set_shader_param("color", color)
 
 func _physics_process(delta):
 	$Pivot.rotation += rotation_speed * delta
@@ -43,6 +49,8 @@ func _physics_process(delta):
 func check_orbits():
 	if abs($Pivot.rotation - orbit_start) > 2 * PI:
 		current_orbits -= 1
+		if settings.enable_sound:
+			$Beep.play()
 		$Label.text = str(current_orbits)
 		if current_orbits <= 0:
 			jumper.die()
@@ -65,7 +73,7 @@ func _draw():
 	if jumper:
 		var r = ((radius - 50) / num_orbits) * (1 + num_orbits - current_orbits)
 		draw_circle_arc_poly(Vector2.ZERO, r + 20, orbit_start + PI/2,
-							 $Pivot.rotation + PI/2, Color(1,0,0))
+							 $Pivot.rotation + PI/2, settings.theme["circle_fill"])
 
 func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	var nb_points := 32
